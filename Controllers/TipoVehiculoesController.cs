a8d1b9add69673b2c08599f342c3d921695f381b
Christian Mora <cmoralopez@gmail.com>
2021-08-04 23:51:50 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/TipoVehiculoesController.cs
@@ -0,0 +1,130 @@
+﻿using Autos.Model;
+using Autos.ModelosNuevos;
+using Autos.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaluacionFinal.Controllers
+{
+    [Authorize]
+    public class TipoVehiculoesController : Controller
+    {
+
+        private readonly EjercicioEvaluacionContext _context;
+        public TipoVehiculoesController(EjercicioEvaluacionContext context)
+        {
+            _context = context;
+        }
+        public void Combox()
+        {
+            ViewData["CodigoVehiculo"] = new SelectList(_context.Vehiculos.Select(x => new ViewModelVehiculoTipoVehiculo
+            {
+                Codigo = x.Codigo,
+                Nombre = x.Nombre,
+                Estado = x.Estado
+            }).Where(x => x.Estado == 1).ToList(), "Codigo", "Nombre", "Descripcion");
+        }
+        [Authorize (Roles = "Owner, Client")]
+        // GET: TipoVehiculoesController
+        public ActionResult Index()
+        {
+            //List<TipoVehiculo> lstTipoVehiculo = _context.TipoVehiculos.ToList();
+            List<ViewModelVehiculoTipoVehiculo> lstTipoVehiculo = _context.TipoVehiculos.Select(x => new ViewModelVehiculoTipoVehiculo
+            {
+                CodigoVehiculo = x.CodigoVehiculo,
+                Codigo = x.Codigo,
+                Nombre = $"{x.CodigoVehiculoNavigation.Nombre}",
+                Estado = x.Estado
+            }).ToList();
+            return View(lstTipoVehiculo);
+        }
+        [Authorize(Roles = "Owner,Client")]
+        // GET: VehiculoesController/Details/5
+        public ActionResult Details(int id)
+        {
+            TipoVehiculo tipoVehiculo = _context.TipoVehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            return View(tipoVehiculo);
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Create
+        public ActionResult Create()
+        {
+            Combox();
+            return View();
+        }
+        [Authorize(Roles = "Owner")]
+        // POST: VehiculoesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TipoVehiculo tipoVehiculo)
+        {
+            try
+            {
+                tipoVehiculo.Estado = 1;
+                _context.Add(tipoVehiculo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                Combox();
+                return View(tipoVehiculo);
+            }
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Combox();
+            TipoVehiculo tipoVehiculo = _context.TipoVehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            return View(tipoVehiculo);
+        }
+        [Authorize(Roles = "Owner")]
+        // POST: VehiculoesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, TipoVehiculo tipoVehiculo)
+        {
+            if (id != tipoVehiculo.Codigo)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                _context.Update(tipoVehiculo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                Combox();
+                return View(tipoVehiculo);
+            }
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Delete/5
+        public ActionResult Desactivar(int id)
+        {
+            TipoVehiculo tipoVehiculo = _context.TipoVehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            tipoVehiculo.Estado = 0;
+            _context.Update(tipoVehiculo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize(Roles = "Owner")]
+        public ActionResult Activar(int id)
+        {
+            TipoVehiculo tipoVehiculo = _context.TipoVehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            tipoVehiculo.Estado = 1;
+            _context.Update(tipoVehiculo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/VehiculoesController.cs
@@ -0,0 +1,106 @@
+﻿using Autos.Model;
+using Autos.ModelosNuevos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaluacionFinal.Controllers
+{
+    [Authorize]
+    public class VehiculoesController : Controller
+    {
+        private readonly EjercicioEvaluacionContext _context;
+        public VehiculoesController(EjercicioEvaluacionContext context)
+        {
+            _context = context;
+        }
+        [Authorize(Roles = "Owner,Client")]
+        // GET: VehiculoesController
+        public ActionResult Index()
+        {
+            List<Vehiculo> lstVehiculo = _context.Vehiculos.ToList();
+            return View(lstVehiculo);
+        }
+        [Authorize(Roles = "Owner,Client")]
+        // GET: VehiculoesController/Details/5
+        public ActionResult Details(int id)
+        {
+            Vehiculo vehiculo = _context.Vehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            return View(vehiculo);
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Owner")]
+        // POST: VehiculoesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Vehiculo vehiculo)
+        {
+            try
+            {
+                _context.Add(vehiculo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Vehiculo vehiculo = _context.Vehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            return View(vehiculo);
+        }
+        [Authorize(Roles = "Owner")]
+        // POST: VehiculoesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Vehiculo vehiculo)
+        {
+            if(id != vehiculo.Codigo)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                _context.Update(vehiculo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(vehiculo);
+            }
+        }
+        [Authorize(Roles = "Owner")]
+        // GET: VehiculoesController/Delete/5
+        public ActionResult Desactivar(int id)
+        {
+            Vehiculo vehiculo = _context.Vehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            vehiculo.Estado = 0;
+            _context.Update(vehiculo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize(Roles = "Owner")]
+        public ActionResult Activar(int id)
+        {
+            Vehiculo vehiculo = _context.Vehiculos.Where(x => x.Codigo == id).FirstOrDefault();
+            vehiculo.Estado = 1;
+            _context.Update(vehiculo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
